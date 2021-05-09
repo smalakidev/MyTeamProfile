2fcccee0819d7e4d1e3b71e7ff4903e1f6ea5426
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-09 13:53:30 -0400

    modified file to be manager.js

--- html.js
+++ html.js
@@ -14,3 +14,15 @@ const render = (employees) => {
 
 	return renderMain(html.join(''));
 };
+
+const renderManager = (manager) => {
+	let template = fs.readFileSync(path.resolve(templatesDir, 'manager.html'), 'utf8');
+	template = replacePlaceholders(template, 'name', manager.getName());
+	template = replacePlaceholders(template, 'role', manager.getRole());
+	template = replacePlaceholders(template, 'email', manager.getEmail());
+	template = replacePlaceholders(template, 'id', manager.getId());
+	template = replacePlaceholders(template, 'officeNumber', manager.getOfficeNumber());
+	return template;
+};
+
+
