7fb1abe54c00b0380c1e8b10b9d449decf025468
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-09 13:28:45 -0400

    added an intern class in the intern js file

---
+++ jsfiles/intern.js
@@ -0,0 +1,9 @@
+//We define and export the intern class here
+
+const Employee = require('./Employee');
+
+class Intern extends Employee {
+	constructor(id, name, email, school) {
+		super(id, name, email);
+		this.school = school;
+	}
