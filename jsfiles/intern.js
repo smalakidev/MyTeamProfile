2b82afe3fe50706b871b6642efb6817a7b0790aa
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-09 13:30:30 -0400

    used get in the rest of the code

--- jsfiles/intern.js
+++ jsfiles/intern.js
@@ -6,4 +6,15 @@ class Intern extends Employee {
 	constructor(id, name, email, school) {
 		super(id, name, email);
 		this.school = school;
+    }
+    
+    getSchool() {
+		return this.school;
 	}
+
+	getRole() {
+		return 'Intern';
+	}
+}
+
+module.exports = Intern;
