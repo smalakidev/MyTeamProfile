5db60fd13dc72961c91ceced412142695c34a481
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-09 13:46:19 -0400

    html.js

--- html.js
+++ html.js
@@ -5,3 +5,12 @@ const fs = require('fs');
 
 const templatesDir = path.resolve(__dirname, '../templates');
 
+const render = (employees) => {
+	const html = [];
+
+	html.push(employees.filter((employee) => employee.getRole() === 'Manager').map((manager) => renderManager(manager)));
+	html.push(employees.filter((employee) => employee.getRole() === 'Engineer').map((engineer) => renderEngineer(engineer)));
+	html.push(employees.filter((employee) => employee.getRole() === 'Intern').map((intern) => renderIntern(intern)));
+
+	return renderMain(html.join(''));
+};
