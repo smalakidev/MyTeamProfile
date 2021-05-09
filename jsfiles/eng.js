cb2466f78297cc00d35dd58279cf5e1afa76753a
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-09 13:16:23 -0400

    got a package.json and used require to define and export the engineer class

---
+++ jsfiles/eng.js
@@ -0,0 +1,20 @@
+//Require is used here
+
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+	constructor(id, name, email, github) {
+		super(id, name, email);
+		this.github = github;
+	}
+
+	getGithub() {
+		return this.github;
+	}
+
+	getRole() {
+		return 'Engineer';
+	}
+}
+
+module.exports = Engineer;
